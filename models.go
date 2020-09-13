d6b38d9fef393b80c5add9adea5a943e18ea82a2
Canberk TURAN <canberkkturann@gmail.com>
2020-09-13 17:00:34 +0300

    Fixed typos of models.go file

--- models.go
+++ models.go
@@ -2,17 +2,17 @@ package main
 
 // User model
 type User struct {
-	Username	string	`json:"username"`
-	Name		string	`json:"name"`
-	Password	string	`json:"password"`
+	Username string `json:"username"`
+	Name     string `json:"name"`
+	Password string `json:"password"`
 }
 
 // Message model
 type Message struct {
-	ID		string	`json:"id"`
-	Sender		string	`json:"sender"`
-	Receiver	string	`json:"receiver"`
-	Content		string	`json:"content"`
-	SendDate	string	`json:"senddate"`
-	ReadDate	string	`json:"readdate"`
+	ID       string `json:"id"`
+	Sender   string `json:"sender"`
+	Receiver string `json:"receiver"`
+	Content  string `json:"content"`
+	SendDate string `json:"senddate"`
+	ReadDate string `json:"readdate"`
 }
