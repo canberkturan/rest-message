7606a98463a12c684e7cc74b22e6953f3c418f16
Canberk TURAN <canberkkturann@gmail.com>
2020-09-13 16:42:30 +0300

    Some linting issues fixed, again.

--- models.go
+++ models.go
@@ -1,11 +1,13 @@
 package main
 
+// User model
 type User struct {
 	Username	string	`json:"username"`
 	Name		string	`json:"name"`
 	Password	string	`json:"password"`
 }
 
+// Message model
 type Message struct {
 	ID		string	`json:"id"`
 	Sender		string	`json:"sender"`
